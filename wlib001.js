f2ac95fca86b131719e2a53da71e9661ab49f6a8
ScarletxZ <horpdorpclop@gmail.com>
2015-02-27 13:23:33 +0800

    Update ver.0.0.2

--- wlib001.js
+++ wlib001.js
@@ -1,11 +1,35 @@
-/* wlib001.js, ver. 0.0.1 */
+/* wlib001.js, ver. 0.0.2 */
 window.wlib = (function() {
  function Wlib (elem) {
- 
+ for(var i=0;i<elem.length;i++) {
+  this[i] = elem[i];
+ }
+ this.length = elem.length;
  }
  
  var wlib = { 
-  /* some proto here */
+  cookie: function() {
+   var setCookie = function(name,value,expires,path) {
+    document.cookie(name,value,expires,path);
+   }
+   var getCookie = function(name) {
+    return document.cookie(name);
+   }
+   var eraseCookie = function(name) {
+    setCookie(name,null,'Thu 01 Jan 1970 GMT 12:00:00','/');
+   }
+  }
+  get: function(selector) {
+   var elem;
+   if(typeof selector === "string") {
+    elem = document.querySelectorAll(selector);
+   }else if (selector.length) {
+    elem = selector;
+   }else{
+    els = [selector];
+   }
+   return new Wlib(elem);
+  }
   };
   
 return wlib;
